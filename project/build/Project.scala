9066e0b9c177d622c0f579018889ab079d2dc49e
Nathan Hamblen <nathan@technically.us>
2010-05-07 09:18:11 -0400

    lift 2.0-M5, appengine api from googlecode repo

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,14 +4,12 @@ class Project(info: ProjectInfo) extends DefaultWebProject(info) {
   val snapshots = ScalaToolsSnapshots  
   // required because Ivy doesn't pull repositories from poms
   val smackRepo = "m2-repository-smack" at "http://maven.reucon.com/public"
-  // ensure that all new lift dependencies are fetched
-  val nexusRepo = "nexus" at "https://nexus.griddynamics.net/nexus/content/groups/public"
+  val appengineRepo = "nexus" at "http://maven-gae-plugin.googlecode.com/svn/repository/"
   
   val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test"
-  val lift = "net.liftweb" % "lift-webkit" % "2.0-SNAPSHOT" % "compile"
+  val lift = "net.liftweb" % "lift-webkit" % "2.0-M5"
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
   val codec = "commons-codec" % "commons-codec" % "1.4"
   
-  val gaeTools = "com.google.appengine" % "appengine-tools-sdk" % "1.3.3.1"
-  val gae = "com.google.appengine" % "appengine" % "1.3.3.1"
+  val gaeTools = "com.google.appengine" % "appengine-tools-api" % "1.3.3.1"
 }
