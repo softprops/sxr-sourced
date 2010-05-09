8c5508b7f07f7c3ee2db858cd6a72311a5556fd3
Nathan Hamblen <nathan@technically.us>
2010-05-09 11:47:53 -0400

    use wholePath to handle no-suffix urls

--- src/main/scala/Stores.scala
+++ src/main/scala/Stores.scala
@@ -3,7 +3,7 @@ package implicitly
 case class Token(secret: String)
 
 object OrgStore  {
-  var cache = Map.empty[String, Token]
+  var cache = Map("us.technically.spde" -> Token("testing"))
   def apply(key: String) = cache.get(key)
   def + (kv: (String, Token)) = cache += kv
 }
