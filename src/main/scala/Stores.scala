e9fcaccd9ee6877d0bc712fd18ba175a02ea7af6
doug tangren <d.tangren@gmail.com>
2010-05-09 17:23:15 -0400

    docs needed to store more data than String could handle

--- src/main/scala/Stores.scala
+++ src/main/scala/Stores.scala
@@ -1,31 +1,32 @@
 package implicitly
  
 import javax.jdo.annotations._
+import com.google.appengine.api.datastore.{Text => BigString}
 
 @PersistenceCapable{val identityType = IdentityType.APPLICATION, val detachable="true"}
 class Token() {
   @PrimaryKey
   @Persistent{val valueStrategy = IdGeneratorStrategy.IDENTITY}
-  var org : String = _
+  var org: String = _
 
   @Persistent
-  var secret : String = ""
+  var secret: String = ""
 }
 
 @PersistenceCapable{val identityType = IdentityType.APPLICATION, val detachable="true"}
 class Doc() {
   @PrimaryKey
   @Persistent{val valueStrategy = IdGeneratorStrategy.IDENTITY}
-  var url : String = _
+  var url: String = _
 
   @Persistent
-  var doc : String = ""
+  var doc: BigString = new BigString("")
 }
 
 object OrgStore extends JdoStore[Token] {
   override val domainCls = classOf[Token]
   type KeyClass = String
-  def apply(key:String) = get(key)
+  def apply(key: String) = get(key)
   def + (kv: (String, Token)) = {
     val t = new Token
     t.org = kv._1
@@ -35,13 +36,14 @@ object OrgStore extends JdoStore[Token] {
 }
 
 object SrcStore extends JdoStore[Doc] {
+  
   override val domainCls = classOf[Doc]
   type KeyClass = String
-  def apply(key:String) = get(key)
+  def apply(key: String) = get(key)
   def + (kv: (String, String)) = {
     val d = new Doc
     d.url = kv._1
-    d.doc = kv._2
+    d.doc = new BigString(kv._2)
     save(d)
   }
 }
