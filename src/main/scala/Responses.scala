8af58873dd94d9e5de03782c7dfc7c72f7de404b
Nathan Hamblen <nathan@technically.us>
2010-05-10 03:12:35 -0400

    adminPage handling, sort of

--- src/main/scala/Responses.scala
+++ src/main/scala/Responses.scala
@@ -3,6 +3,7 @@ package implicitly
 /** Custom Response types */
 trait Responses {
   import net.liftweb.http._
+  import net.liftweb.http.provider.servlet.HTTPRequestServlet
   
   /** A `looser` response for source documents */
   case class SrcResponse(src: String, contentType: String, headers: List[(String, String)], code: Int) extends LiftResponse {
@@ -15,6 +16,15 @@ trait Responses {
   case class SrcCreatedResponse(loc: String, mime: String) extends LiftResponse {
     def toResponse = InMemoryResponse(Array(), ("Location" -> loc) :: ("Content-Type" -> mime) :: Nil, Nil, 201)
   }
-  def adminResponse(out: scala.xml.Node) =
-    XhtmlResponse(out, net.liftweb.common.Empty, List("Content-Type" -> "text/html; charset=utf-8"), Nil, 200, false)
+  def adminPage(req: Req)(out: => scala.xml.Node) = {
+    import net.liftweb.common.Full
+    req.request match {
+      case r: HTTPRequestServlet if r.req.getUserPrincipal != null => 
+        println(r.req.getUserPrincipal.getName)
+        XhtmlResponse(out, net.liftweb.common.Empty, List("Content-Type" -> "text/html; charset=utf-8"), Nil, 200, false)
+      case _ => 
+        val svc = com.google.appengine.api.users.UserServiceFactory.getUserService
+        RedirectResponse(svc.createLoginURL("http://localhost:8080/admin"))
+    }
+  }
 }
