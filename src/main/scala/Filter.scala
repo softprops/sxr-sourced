62e81e136644ac290ce333411b05d84ce9c9a51c
Nathan Hamblen <nathan@technically.us>
2010-05-10 22:00:38 -0400

    custom filter to pass on /_ah/ requests

---
+++ src/main/scala/Filter.scala
@@ -0,0 +1,13 @@
+package implicitly
+import javax.servlet.{ServletRequest, ServletResponse, FilterChain}
+import javax.servlet.http.HttpServletRequest
+
+class Filter extends net.liftweb.http.provider.servlet.ServletFilterProvider {
+  override def doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) = {
+    req match {
+      case req: HttpServletRequest if req.getRequestURI.startsWith("/_ah/") =>
+        chain.doFilter(req, res)
+      case _ => super.doFilter(req, res, chain)
+    }
+  }
+}
