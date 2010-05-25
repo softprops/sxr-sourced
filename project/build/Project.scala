59c4d8153ba808dec940348c682c274bd76583a9
doug tangren <d.tangren@gmail.com>
2010-05-24 20:09:48 -0400

    rm lift added specs

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,6 +3,7 @@ import sbt._
 class Project(info: ProjectInfo) extends AppengineProject(info) with DataNucleus {
   //val snapshots = ScalaToolsSnapshots  
   val snapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1-SNAPSHOT" % "test"
   
   // required because Ivy doesn't pull repositories from poms
   val smackRepo = "m2-repository-smack" at "http://maven.reucon.com/public"
@@ -14,9 +15,6 @@ class Project(info: ProjectInfo) extends AppengineProject(info) with DataNucleus
   // unfiltered
   val uf = "net.databinder" %% "unfiltered" % "0.1.1-SNAPSHOT"
   val ufs = "net.databinder" %% "unfiltered-server" % "0.1.1-SNAPSHOT"
-  
-  // lift
-  val lift = "net.liftweb" % "lift-webkit" % "2.0-M5"
   
   // security
   val codec = "commons-codec" % "commons-codec" % "1.4"
