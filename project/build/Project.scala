583a47f75752f52194959dab9579f400d6e22df8
softprops <d.tangren@gmail.com>
2010-08-11 21:50:44 -0400

    update specs dep

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,7 +3,12 @@ import sbt._
 class Project(info: ProjectInfo) extends AppengineProject(info) with DataNucleus {
   //val snapshots = ScalaToolsSnapshots  
   val snapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+  lazy val specs = specsDependency
+  def specsDependency =
+    if (buildScalaVersion startsWith "2.7.")
+      "org.scala-tools.testing" % "specs" % "1.6.2.2"
+    else
+      "org.scala-tools.testing" %% "specs" % "1.6.5"
   
   // required because Ivy doesn't pull repositories from poms
   val smackRepo = "m2-repository-smack" at "http://maven.reucon.com/public"
