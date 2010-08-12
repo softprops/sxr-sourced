f58ca8053e1a926c947536f7aa15e346fc356890
softprops <d.tangren@gmail.com>
2010-08-11 21:51:49 -0400

    updated auth spec

--- src/test/scala/AuthSpec.scala
+++ src/test/scala/AuthSpec.scala
@@ -7,11 +7,8 @@ object AuthSpec extends Specification with Auth {
     "sign a request" in {
       val secret = "09vy9uOblAm/XNpJXWqtCuOmPnog93P2UsuVPEsx"
       val path = "http://localhost:8080/us.technically.spde/spde-core/0.3.1-SNAPSHOT/linked.js"
-      val content = scala.io.Source.fromFile(
-        new java.io.File("src/test/resources/linked.js")
-      ).getLines.mkString("").getBytes
-      val sig = java.net.URLDecoder.decode("Trd0QZEjE60a%2FcQUI2a5575JY2I%3D")
-      sign(secret, path, content) must_== sig
+      val sig = java.net.URLDecoder.decode("Trd0QZEjE60a%2FcQUI2a5575JY2I%3D", "utf8")
+      sign(secret, path, new java.io.FileInputStream(new java.io.File("src/test/resources/linked.js"))) must_== sig   
     }
   }
 }
