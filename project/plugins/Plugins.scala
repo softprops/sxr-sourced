c10672ac9729b9b14b99f03a34ef87a2e700d9e2
Nathan Hamblen <nathan@technically.us>
2011-04-16 23:02:52 -0400

    plugin vers

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  override def managedStyle = ManagedStyle.Maven
-  val gae = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.1" from "http://github.com/downloads/Yasushi/sbt-appengine-plugin/sbt-appengine-plugin-2.1.jar"
+  val gae = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.2"
 }
